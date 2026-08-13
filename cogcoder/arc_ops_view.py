a9c11ea911a8830582383e1e3606e66fe1912c8d
Nolane-x <casioreview20@gmail.com>
2026-08-14 06:51:34 +0700

    feat: execute row-column context rules

--- cogcoder/arc_ops_view.py
+++ cogcoder/arc_ops_view.py
@@ -5,10 +5,13 @@ from .grid_fold import combine_with_view
 from .local_grid_v2 import rewrite_sparse as rewrite_sparse_v2
 
 
+_V2_LOCAL_KINDS=('orth_tuple','neighbor_tuple','axis_set','axis_counts')
+
+
 def apply_step(step: Step, grid):
     if step.op=='view_combine':
         return combine_with_view(grid,str(step.args[0]))
-    if step.op=='local_rewrite' and str(step.args[0]) in ('orth_tuple','neighbor_tuple'):
+    if step.op=='local_rewrite' and str(step.args[0]) in _V2_LOCAL_KINDS:
         return rewrite_sparse_v2(grid,str(step.args[0]),step.args[1])
     return apply_base_step(step,grid)
 
