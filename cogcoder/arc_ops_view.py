9c765d90f6becfb71c8801fe25d506164e7e0f57
Nolane-x <casioreview20@gmail.com>
2026-08-14 06:56:02 +0700

    feat: execute object-property rewrites

--- cogcoder/arc_ops_view.py
+++ cogcoder/arc_ops_view.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .arc_ops import Program, Step, apply_program as apply_base_program, apply_step as apply_base_step
 from .grid_fold import combine_with_view
 from .local_grid_v2 import rewrite_sparse as rewrite_sparse_v2
+from .object_grid import rewrite_objects
 
 
 _V2_LOCAL_KINDS=('orth_tuple','neighbor_tuple','axis_set','axis_counts')
@@ -13,6 +14,8 @@ def apply_step(step: Step, grid):
         return combine_with_view(grid,str(step.args[0]))
     if step.op=='local_rewrite' and str(step.args[0]) in _V2_LOCAL_KINDS:
         return rewrite_sparse_v2(grid,str(step.args[0]),step.args[1])
+    if step.op=='object_rewrite':
+        return rewrite_objects(grid,str(step.args[0]),step.args[1])
     return apply_base_step(step,grid)
 
 
