881ee9f8eb98e06b8c37670a9d3d18910764e75a
Nolane-x <casioreview20@gmail.com>
2026-08-19 23:52:04 +0700

    feat(r269): allow verifier-backed meta episode portable experiences

--- cogcoder/r269_causal_basis_adapter.py
+++ cogcoder/r269_causal_basis_adapter.py
@@ -11,6 +11,7 @@ from .r256_operator_dsl import Binary, Const, Expr, Field, IfElse, Unary
 
 _ACCEPTED_R268_PARENT = 'fda7f502185266fedb00886d5786c6d28cc0e0eb'
 _ALLOWED_ADAPTATION_OPS = ('add', 'sub', 'mul', 'div', 'min', 'max')
+_ALLOWED_ADAPTER_TYPES = frozenset(('causal_basis_v1', 'verified_meta_episode_v1'))
 _CANONICAL_ROLE_RE = re.compile(r'^__r([0-9]+)$')
 
 
@@ -129,7 +130,7 @@ class PortableExperience:
     trainable_parameter_count: int = 0
 
     def __post_init__(self) -> None:
-        if self.adapter_type != 'causal_basis_v1':
+        if self.adapter_type not in _ALLOWED_ADAPTER_TYPES:
             raise ValueError('unsupported adapter_type')
         if not isinstance(self.canonical_expression, Expr):
             raise TypeError('canonical_expression must be Expr')
