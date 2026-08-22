09fd0830d6ab05121ce9128dc10bd3ac7af012b9
Nolane-x <casioreview20@gmail.com>
2026-08-22 13:26:48 +0700

    feat: open Coding AGI organization package

---
+++ cogcoder/organization/__init__.py
@@ -0,0 +1,33 @@
+"""Nolane Coding AGI organization foundation.
+
+This package provides model-agnostic organizational substrate. Configuring an
+identity here is not evidence that the underlying neural system is AGI.
+"""
+
+from .types import (
+    AgentIdentity,
+    AgentRank,
+    AgentStatus,
+    CognitiveEvent,
+    ContextCapsule,
+    EvidenceRecord,
+    EventKind,
+    MemoryEntry,
+    MemoryScope,
+    ParameterAccounting,
+    SkillScope,
+)
+
+__all__ = [
+    'AgentIdentity',
+    'AgentRank',
+    'AgentStatus',
+    'CognitiveEvent',
+    'ContextCapsule',
+    'EvidenceRecord',
+    'EventKind',
+    'MemoryEntry',
+    'MemoryScope',
+    'ParameterAccounting',
+    'SkillScope',
+]
