d04009b7c78d281b0133c8d4599e8ea3bc639072
Nolane-x <casioreview20@gmail.com>
2026-08-24 07:41:43 +0700

    refactor(refoundation): add canonical schemas package

---
+++ nolane/schemas/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from .identity import (
+    PHYSICAL_PARAMETER_CEILING,
+    AgentIdentity,
+    AgentRank,
+    AgentStatus,
+    ParameterAccounting,
+)
+
+__all__ = (
+    "PHYSICAL_PARAMETER_CEILING",
+    "AgentRank",
+    "AgentStatus",
+    "ParameterAccounting",
+    "AgentIdentity",
+)
