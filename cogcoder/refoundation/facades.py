ce8e609ec0dece0c7e227fb6f54b3b00d3560922
Nolane-x <casioreview20@gmail.com>
2026-08-24 18:54:18 +0700

    refactor: retire Architecture compatibility facade

--- cogcoder/refoundation/facades.py
+++ cogcoder/refoundation/facades.py
@@ -60,7 +60,6 @@ def build_active_facade_bindings() -> tuple[FacadeBinding, ...]:
     return (
         FacadeBinding("external.context", "nolane.memory.context", "cogcoder.organization.memory_context", ("MemoryContextControlPlane",)),
         FacadeBinding("external.individual_evolution", "nolane.external_core.individual_evolution", "cogcoder.organization.individual_evolution", ("IndividualEvolutionControlPlane",)),
-        FacadeBinding("external.architecture", "nolane.external_core.architecture", "cogcoder.organization.architecture", ("ArchitectureControlPlane",)),
         FacadeBinding("external.integration", "nolane.external_core.integration", "cogcoder.organization.integration", ("IntegrationControlPlane",)),
         FacadeBinding("external.coding.control", "nolane.external_core.coding", "cogcoder.organization.coding", ("CodingControlPlane",)),
         FacadeBinding("external.debugging", "nolane.external_core.debugging", "cogcoder.organization.debugging", ("DebugControlPlane",)),
