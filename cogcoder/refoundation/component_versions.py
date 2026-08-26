cdc2063e91017db128a4e765798743087bfa2743
Nolane-x <casioreview20@gmail.com>
2026-08-26 08:04:32 +0700

    feat(refoundation): advance executor component revision

--- cogcoder/refoundation/component_versions.py
+++ cogcoder/refoundation/component_versions.py
@@ -39,6 +39,7 @@ _COMPONENT_REVISIONS.update(
         "external.integration": 1,
         "external.invokable_cores": 1,
         "external.execution.workspace": 1,
+        "external.execution.executor": 1,
         "external.coding.claims": 1,
         "external.coding.patches": 1,
     }
