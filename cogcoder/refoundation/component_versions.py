1666ff8b05cf32421699f60448bff668e509e53a
Nolane-x <casioreview20@gmail.com>
2026-08-27 14:52:01 +0700

    Refoundation Wave 5AF: advance evaluation-regimes component version

--- cogcoder/refoundation/component_versions.py
+++ cogcoder/refoundation/component_versions.py
@@ -47,6 +47,7 @@ _COMPONENT_REVISIONS.update(
         "external.coding.control": 1,
         "external.debugging": 1,
         "external.ui_ux": 1,
+        "evaluation.regimes": 1,
         "neural.inference_bridge": 1,
     }
 )
