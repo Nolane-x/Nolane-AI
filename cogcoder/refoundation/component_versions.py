3dbcf834d423b31b7a47b7c8bccf92b0cb5a31d3
Nolane-x <casioreview20@gmail.com>
2026-08-28 21:31:36 +0700

    refoundation: advance external.causal component version

--- cogcoder/refoundation/component_versions.py
+++ cogcoder/refoundation/component_versions.py
@@ -28,6 +28,7 @@ _COMPONENT_REVISIONS.update(
         "external.operations": 1,
         "external.research": 1,
         "external.cognitive_library": 1,
+        "external.causal": 1,
         "external.verification": 1,
         "external.evidence": 1,
         "external.experience": 1,
