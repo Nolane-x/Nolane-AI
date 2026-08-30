aba74b305931a8477c87d4ee36a639d76c41361f
Nolane-x <casioreview20@gmail.com>
2026-08-30 13:43:44 +0700

    compat(goal-design): expose live authority runtime

--- cogcoder/organization/goal_design.py
+++ cogcoder/organization/goal_design.py
@@ -3,3 +3,4 @@
 from nolane.external_core.goal_design import *  # noqa: F401,F403
 from nolane.external_core.goal_design_contracts import *  # noqa: F401,F403
 from nolane.external_core.goal_design_ledger import *  # noqa: F401,F403
+from nolane.external_core.goal_design_runtime import *  # noqa: F401,F403
