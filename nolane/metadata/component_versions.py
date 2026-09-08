85b1b9622d396b6f1d4e4f605bc118339162aaa6
Nolane-x <casioreview20@gmail.com>
2026-09-08 12:49:16 +0700

    chore(execution): advance canonical control revision

--- nolane/metadata/component_versions.py
+++ nolane/metadata/component_versions.py
@@ -52,7 +52,7 @@ _COMPONENT_REVISIONS.update(
         "external.invokable_cores": 3,
         "external.execution.workspace": 4,
         "external.execution.executor": 3,
-        "external.execution.control": 13,
+        "external.execution.control": 14,
         "external.coding.claims": 2,
         "external.coding.patches": 2,
         "external.coding.control": 1,
