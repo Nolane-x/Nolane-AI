3e784de7feeb7f0da110475f5a82806838b54ba9
Nolane-x <casioreview20@gmail.com>
2026-09-11 22:01:24 +0700

    chore(context): advance external context component version

--- nolane/external_core/context.py
+++ nolane/external_core/context.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from nolane.memory.skills import SkillEvolutionEngine
 
 COMPONENT_ID = "external.context"
-COMPONENT_VERSION = "0.0.2"
+COMPONENT_VERSION = "0.0.3"
 MIGRATED_FROM = "cogcoder.organization.context + cogcoder.organization.types"
 
 
