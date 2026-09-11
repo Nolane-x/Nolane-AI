b7ae8e560910bfb4844c75c7f7095a9e9c53e032
Nolane-x <casioreview20@gmail.com>
2026-09-11 22:02:23 +0700

    chore(context): register external context revision 3

--- nolane/metadata/component_versions.py
+++ nolane/metadata/component_versions.py
@@ -48,7 +48,7 @@ _COMPONENT_REVISIONS.update(
         "external.planning": 1,
         "external.architecture": 1,
         "external.integration": 10,
-        "external.context": 2,
+        "external.context": 3,
         "external.invokable_cores": 3,
         "external.execution.workspace": 5,
         "external.execution.executor": 4,
