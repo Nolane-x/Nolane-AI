4e5540edde5b98c095f7d9a868cadc4af0fcea87
Nolane-x <casioreview20@gmail.com>
2026-09-12 21:00:09 +0700

    chore(neural): advance architecture provenance revision

--- nolane/external_core/architecture.py
+++ nolane/external_core/architecture.py
@@ -8,7 +8,7 @@ from nolane.organization.events import EventKind
 from nolane.core.canonical_digest import canonical_digest
 
 COMPONENT_ID = "external.architecture"
-COMPONENT_VERSION = "0.0.1"
+COMPONENT_VERSION = "0.0.2"
 MIGRATED_FROM = "cogcoder.organization.architecture"
 
 
