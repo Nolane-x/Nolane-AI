6c129ff90ed039941429432dd25023a52576437f
Nolane-x <casioreview20@gmail.com>
2026-09-18 11:58:56 +0700

    chore: advance evaluation.release canonical version

--- nolane/evaluation/release.py
+++ nolane/evaluation/release.py
@@ -339,5 +339,5 @@ class EvaluationReleaseLedger:
 
 
 COMPONENT_ID = "evaluation.release"
-COMPONENT_VERSION = "0.0.1"
+COMPONENT_VERSION = "0.0.2"
 MIGRATED_FROM = "cogcoder.organization.evaluation_release"
