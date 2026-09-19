e50f83bb22332549dbf31eda76ad3456ee879302
Nolane-x <casioreview20@gmail.com>
2026-09-19 08:55:37 +0700

    neural-vnext: add multi-depth candidate package

---
+++ model/neural-vnext/nvnext/__init__.py
@@ -0,0 +1,15 @@
+from .multidepth import (
+    AdaptiveDepthCandidate,
+    MultiDepthLossConfig,
+    MultiDepthObjective,
+    first_stable_correct_step,
+    ponder_continue_targets,
+)
+
+__all__ = (
+    "AdaptiveDepthCandidate",
+    "MultiDepthLossConfig",
+    "MultiDepthObjective",
+    "first_stable_correct_step",
+    "ponder_continue_targets",
+)
