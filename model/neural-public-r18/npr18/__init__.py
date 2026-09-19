d8eb17102760620302f0005634d85a27b5d119dc
Nolane-x <casioreview20@gmail.com>
2026-09-19 11:02:02 +0700

    feat(neural): export public R18 recovery candidate

---
+++ model/neural-public-r18/npr18/__init__.py
@@ -0,0 +1,40 @@
+from .core import (
+    PublicR18Architecture,
+    PublicR18RecursiveCore,
+    encode_public_actions,
+    encode_public_text,
+    public_r18_parameter_count,
+)
+from .data import (
+    PublicTeacherEpisode,
+    PublicTeacherStep,
+    collect_public_teacher_corpus,
+    collect_public_teacher_episode,
+    tensorize_public_step,
+)
+from .evaluation import evaluate_public_r18, run_public_episode
+from .training import (
+    CHECKPOINT_FORMAT,
+    load_public_r18_checkpoint,
+    save_public_r18_checkpoint,
+    train_public_r18_epoch,
+)
+
+__all__ = (
+    "CHECKPOINT_FORMAT",
+    "PublicR18Architecture",
+    "PublicR18RecursiveCore",
+    "PublicTeacherEpisode",
+    "PublicTeacherStep",
+    "collect_public_teacher_corpus",
+    "collect_public_teacher_episode",
+    "encode_public_actions",
+    "encode_public_text",
+    "evaluate_public_r18",
+    "load_public_r18_checkpoint",
+    "public_r18_parameter_count",
+    "run_public_episode",
+    "save_public_r18_checkpoint",
+    "tensorize_public_step",
+    "train_public_r18_epoch",
+)
