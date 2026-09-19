5feb4d5d78bf54dc46abca8de4a3ece41d2f6aa5
Nolane-x <casioreview20@gmail.com>
2026-09-19 08:58:42 +0700

    neural-vnext: export reproducible training surfaces

--- model/neural-vnext/nvnext/__init__.py
+++ model/neural-vnext/nvnext/__init__.py
@@ -5,11 +5,23 @@ from .multidepth import (
     first_stable_correct_step,
     ponder_continue_targets,
 )
+from .training import (
+    VerifiedMultiDepthTargets,
+    configure_vnext_training,
+    make_vnext_optimizer,
+    sample_multidepth_steps,
+    train_vnext_step,
+)
 
 __all__ = (
     "AdaptiveDepthCandidate",
     "MultiDepthLossConfig",
     "MultiDepthObjective",
+    "VerifiedMultiDepthTargets",
+    "configure_vnext_training",
     "first_stable_correct_step",
+    "make_vnext_optimizer",
     "ponder_continue_targets",
+    "sample_multidepth_steps",
+    "train_vnext_step",
 )
